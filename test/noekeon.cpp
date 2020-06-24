ed6a56909332e848104d42bcea1450bb2d0df874
nkaskov <nikitakaskov@mail.ru>
2020-06-24 08:13:22 -0400

    Basic Noekeon test added #35

--- test/noekeon.cpp
+++ test/noekeon.cpp
@@ -27,13 +27,14 @@ BOOST_AUTO_TEST_SUITE(noekeon_test_suite)
 
 BOOST_AUTO_TEST_CASE(noekeon_1) {
 
-    std::vector<char> input = {'\xea', '\x02', '\x47', '\x14', '\xad', '\x5c', '\x4d', '\x84'};
+    std::vector<char> input = {'\x52', '\xf8', '\x8a', '\x7b', '\x28', '\x3c', '\x1f', '\x7b', 
+    							'\xdf', '\x7b', '\x6f', '\xaa', '\x50', '\x11', '\xc7', '\xd8'};
     std::vector<char> key = {'\xba', '\x69', '\x33', '\x81', '\x92', '\x99', '\xc7', '\x16',
                              '\x99', '\xa9', '\x9f', '\x08', '\xf6', '\x78', '\x17', '\x8b'};
 
     std::string out = encrypt<block::noekeon>(input, key);
 
-    BOOST_CHECK_EQUAL(out, "df1f9b251c0bf45f");
+    BOOST_CHECK_EQUAL(out, "5096f2bfc82ae6e2d9495515c277fa70");
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()		
